e5e6acb41004c9df62c0549525aeef81b11ad78a
student <student@ai251lab0219m11.aicala.edmc.stu>
2014-12-10 16:08:58 -0800

    trying to commit

---
+++ public/js/ag-controllers.js
@@ -0,0 +1,33 @@
+/*
+*@ namespace ag-controllers.js
+*/
+
+var WeVidHereConfig = angular.module('WeVidHere',[]);
+
+WeVidHereConfig.controller (
+	'WeVidHereConfig', 
+	['$scope', // the object we attach everything to
+	'$http',   // imported Ajax object, injected into our controller
+		function ($scope, $http){
+			console.log("help me spock");
+			//get data by Ajax
+
+			$http(
+				method: "GET",
+				url:     "data/users.json"
+
+			)
+			.success(function (data, status, headers, config){
+				//do something if we got the data
+				}
+			);
+			// check data on submit form
+			$scope.doLogin = function(){
+				// process what user typed in
+
+			}
+
+		}
+
+    ]
+);
